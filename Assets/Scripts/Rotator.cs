08372f5757166abab41b57391d0e65ed0d63d985
20shaked20 <shakedlevi32@gmail.com>
2024-01-12 13:26:01 +0200

    Rotator done

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+
+    [SerializeField]
+    Vector3 rotation_axis;
+
+    [SerializeField]
+    float rotation_velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Object rotates on y axis");
+        rotation_axis.y = rotation_velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         transform.eulerAngles += rotation_axis * Time.deltaTime;
     }
 }
