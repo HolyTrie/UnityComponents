cd074ef3ec7c62ebb8923d02eaa4625e95d572d7
Yonatan Ratner <yonaratner@gmail.com>
2024-01-12 17:46:04 +0200

    +quaternion yapping

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -30,12 +30,21 @@ public class Oscillator : MonoBehaviour
             we really struggled with this one, finally we found this solution which uses unity Quaternions
             instead of localRotation or RigidBody.
             Users 'Awesome2819' comment in https://discussions.unity.com/t/problem-with-oscillation/23029/3
+
+            eulerangles are also viable for rotation in Unity, but these get translated 
+            into Quaternions behind the scenes anyway, so this may be good practice.
+
         */
         _x = transform.rotation.x;
         _y = transform.rotation.y;
         _z = amplitude * Mathf.Sin(Time.time * omega);
         UnityEngine.Quaternion _rotation = UnityEngine.Quaternion.Euler(_x,_y,_z);
         transform.rotation = _rotation;
+
+        /*
+        this solution can be a one liner as well
+        transform.rotation = UnityEngine.Quaternion.Euler(transform.rotation.x,transform.rotation.y,amplitude * Mathf.Sin(Time.time * omega));
+        */
     }
     
 }
